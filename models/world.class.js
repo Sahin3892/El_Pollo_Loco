8e46299ec2fadfbbbea4b8e2e58bf2b874448176
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2023-11-18 22:00:25 +0100

    AutoLoad IMG

--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,37 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
+  ctx;
 
-  draw() {}
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height
+    );
+    enemies.forEach(enemie => {
+      this.ctx.drawImage(
+        enemie.img,
+        enemie.x,
+        enemie.y,
+        enemie.width,
+        enemie.height
+      );
+    });
+
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
 }
