96e1e21ba93b586ae9676ef96fdc4bf1582b4adb
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2023-12-05 17:59:22 +0100

    Steuerung Links Rechts vom Character Implementiert

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,6 @@
 class Character extends MoveableObject {
   height = 200;
+  speed = 10;
   IMAGES_WALKING = [
     "img/2_character_pepe/2_walk/W-21.png",
     "img/2_character_pepe/2_walk/W-22.png",
@@ -9,7 +10,7 @@ class Character extends MoveableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
   world;
-  
+
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
@@ -18,12 +19,28 @@ class Character extends MoveableObject {
 
   animate() {
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6;
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
-    }, 100);
-  }
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+        this.otherDirection = false;
+      }
 
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+        this.otherDirection = true;
+      }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT ||this.world.keyboard.LEFT) {
+        this.x += this.speed;
+
+        // Walk animation
+        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 30);
+  }
   jump() {}
 }
--- models/world.class.js
+++ models/world.class.js
@@ -20,7 +20,7 @@ class World {
     this.setWorld();
   }
 
-  setWorld(){
+  setWorld() {
     this.character.world = this;
   }
 
@@ -48,6 +48,17 @@ class World {
   }
 
   addToMap(mo) {
+    if (mo.otherDirection) {
+      this.ctx.save();
+      this.ctx.translate(mo.width, 0);
+      this.ctx.scale(-1, 1);
+      mo.x = mo.x * -1;
+    }
+    
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    if (mo.otherDirection) {
+      mo.x = mo.x * -1;
+      this.ctx.restore();
+    }
   }
 }
