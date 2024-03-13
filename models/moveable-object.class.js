66e2a28c2dbd1da194251ea0c4f1b829d692bc5f
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2024-03-13 15:45:01 +0100

    Cleaned Code

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,58 +1,72 @@
 class MoveableObject {
-  x = 120;
-  y = 230;
-  img;
-  height = 150;
-  width = 100;
-  imageCache = {};
-  currentImage = 0;
-  speed = 0.15;
-  otherDirection = false;
-  speedY = 0;
-  acceleration = 2.5;
+    x = 120;
+    y = 230;
+    img;
+    height = 150;
+    width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.15;
+    otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
 
-  applyGravity() {
-    setInterval(() => {
-      if (this.isAboveGround() || this.speedY > 0) {
-        this.y -= this.speedY;
-        this.speedY -= this.acceleration;
-      }
-    }, 1000 / 25);
-  }
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
 
-  isAboveGround() {
-    return this.y < 230;
-  }
+    isAboveGround() {
+        return this.y < 230;
+    }
 
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
-  /**
-   *
-   * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
-   */
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
-  }
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
 
-  playAnimation(images) {
-    // Walk animation
-    let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6;
-    let path = images[i];
-    this.img = this.imageCache[path];
-    this.currentImage++;
-  }
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
 
-  moveRight() {
-    this.x += this.speed;
-  }
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = "5";
+        ctx.strokeStyle = "blue";
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
 
-  moveLeft() {
-    this.x -= this.speed;
-  }
+    }
+
+    /**
+     *
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
+    playAnimation(images) {
+        // Walk animation
+        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
+    moveRight() {
+        this.x += this.speed;
+    }
+
+    moveLeft() {
+        this.x -= this.speed;
+    }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,69 +1,75 @@
 class World {
-  character = new Character();
-  level = level1;
-  canvas;
-  ctx;
-  keyboard;
-  camera_x = 19;
+    character = new Character();
+    level = level1;
+    canvas;
+    ctx;
+    keyboard;
+    camera_x = 19;
 
-  constructor(canvas, keyboard) {
-    this.ctx = canvas.getContext("2d");
-    this.canvas = canvas;
-    this.keyboard = keyboard;
-    this.draw();
-    this.setWorld();
-  }
-
-  setWorld() {
-    this.character.world = this;
-  }
-
-  draw() {
-    // Löschen erst die Map
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    this.ctx.translate(this.camera_x, 0);
-
-    // Objekte werden Hinzugefügt
-    this.addObjectsToMap(this.level.backgroundObjects);
-
-    this.addToMap(this.character);
-    this.addObjectsToMap(this.level.clouds);
-    this.addObjectsToMap(this.level.enemies);
-
-    this.ctx.translate(-this.camera_x, 0);
-
-    // Draw() wird immer wieder ausgeführt
-    let self = this;
-    requestAnimationFrame(function () {
-      self.draw();
-    });
-  }
-
-  addObjectsToMap(objects) {
-    objects.forEach((o) => {
-      this.addToMap(o);
-    });
-  }
-
-  addToMap(mo) {
-    if (mo.otherDirection) {
-      this.ctx.save();
-      this.ctx.translate(mo.width, 0);
-      this.ctx.scale(-1, 1);
-      mo.x = mo.x * -1;
+    constructor(canvas, keyboard) {
+        this.ctx = canvas.getContext("2d");
+        this.canvas = canvas;
+        this.keyboard = keyboard;
+        this.draw();
+        this.setWorld();
     }
 
-    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
-    this.ctx.beginPath();
-    this.ctx.lineWidth = "5";
-    this.ctx.strokeStyle = "blue";
-    this.ctx.rect(mo.x, mo.y, mo.x + mo.width, mo.y + mo.height);
-    this.ctx.stroke();
-
-    if (mo.otherDirection) {
-      mo.x = mo.x * -1;
-      this.ctx.restore();
+    setWorld() {
+        this.character.world = this;
     }
-  }
+
+    draw() {
+        // Löschen erst die Map
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.translate(this.camera_x, 0);
+
+        // Objekte werden Hinzugefügt
+        this.addObjectsToMap(this.level.backgroundObjects);
+
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.level.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
+
+        // Draw() wird immer wieder ausgeführt
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach((o) => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        if (mo.otherDirection) {
+            this.flipImage(mo);
+        }
+        
+        mo.draw(this.ctx);
+        mo.drawFrame(this.ctx);
+
+        if (mo.otherDirection) {
+            this.flipImageBack(mo);
+        }
+    }
+
+    flipImage(mo) {
+        this.ctx.save();
+        this.ctx.translate(mo.width, 0);
+        this.ctx.scale(-1, 1);
+        mo.x = mo.x * -1;
+    }
+
+    flipImageBack(mo) {
+        mo.x = mo.x * -1;
+        this.ctx.restore();
+    }
+
 }
+
