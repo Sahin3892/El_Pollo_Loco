c21493d96e36c93b2d2f9fa36ba7b91302ef0a19
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2024-03-13 20:00:45 +0100

    Collision Detection Integrated

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -34,15 +34,23 @@ class MoveableObject {
     }
 
     drawFrame(ctx) {
-        ctx.beginPath();
-        ctx.lineWidth = "5";
-        ctx.strokeStyle = "blue";
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.stroke();
-
+        if(this instanceof Character  || this instanceof Chicken || this instanceof Endboss) {
+            ctx.beginPath();
+            ctx.lineWidth = "5";
+            ctx.strokeStyle = "blue";
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
     }
 
-    /**
+// Bessere Formel zur Kollisionsberechnung (Genauer)
+isColliding (mo) {
+        return this.x + this.width > mo.x &&
+            this.y + this.height > mo.y &&
+            this.x < mo.x &&
+            this.y < mo.y + mo.height
+}
+/**
      *
      * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
      */
