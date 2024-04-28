1b0550e60827be947a999bab5b2a7998e2544718
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2024-04-28 14:26:12 +0200

    Added scriptjs endboss status bar

---
+++ models/status-bar-endboss.class.js
@@ -0,0 +1,27 @@
+class StatusBarEndbossClass extends DrawableObject {
+
+    IMAGE_ENDBOSS_HEALTH = [
+        "img/7_statusbars/2_statusbar_endboss/green.png"
+    ];
+    constructor() {
+        super();
+        this.health = 100;
+    }
+    draw(ctx) {
+        super.draw(ctx);
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x, this.y - 10, this.health, 5);
+    }
+    updateHealth(value) {
+        this.health += value;
+    }
+    updatePosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    loadImage(path) {
+        // implementation
+        super.loadImage(this.IMAGE_ENDBOSS_HEALTH);
+    }
+
+}
