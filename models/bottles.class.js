98161e57a92eb3e55b5554c353d0d47f0602c0c1
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2024-05-20 20:50:01 +0200

    Testing the Class

--- models/bottles.class.js
+++ models/bottles.class.js
@@ -1,7 +1,5 @@
 class BottlesClass extends DrawableObject {
 
-    scaleFactorX = 0.4;
-    scaleFactorY = 0.7;
     width = 60;
     height = 60;
     offset = {
@@ -13,7 +11,8 @@ class BottlesClass extends DrawableObject {
 
 
     constructor() {
-        super().loadImages(["img/6_salsa_bottle/2_salsa_bottle_on_ground.png"]);
+        super();
+        this.loadImages(["img/6_salsa_bottle/2_salsa_bottle_on_ground.png"]);
         this.x = 400 + Math.random() * 500;
         this.y = 390 - Math.random() * 30;
     }
