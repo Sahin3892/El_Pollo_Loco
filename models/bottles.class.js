1bc89384729b85b25e48dc239c17b8a685c0342b
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2024-06-30 22:03:36 +0200

    Update bottles.class.js

--- models/bottles.class.js
+++ models/bottles.class.js
@@ -6,10 +6,15 @@ class Bottle extends DrawableObject {
 
     constructor() {
         super();
+        this.addBottleToMap();
+
+
+    }
+
+    addBottleToMap() {
         this.loadImage(["img/6_salsa_bottle/2_salsa_bottle_on_ground.png"]);
         this.x = 400 + Math.random() * 500;
         this.y = 330 - Math.random() * 30;
+
     }
-
-
 }
